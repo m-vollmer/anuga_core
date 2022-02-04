d6dae84b5ec7695888a71a4f4a170dc8698247b4
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2022-02-04 12:01:00 +1100

    Beginnings of a spiral wall

--- examples/simple_examples/spiral_wall.py
+++ examples/simple_examples/spiral_wall.py
@@ -6,7 +6,7 @@ Water flowing along a spiral wall and draining into a hole in the centre.
 #------------------------------------------------------------------------------
 # Import necessary modules
 #------------------------------------------------------------------------------
-from math import acos, cos, sin, sqrt
+from math import acos, cos, sin, sqrt, pi
 import anuga
 
 
@@ -15,7 +15,7 @@ import anuga
 #------------------------------------------------------------------------------
 length = 10.
 width = 10.
-dx = dy = 0.07           # Resolution: Length of subdivisions on both axes
+dx = dy = 0.05           # Resolution: Length of subdivisions on both axes
 
 # Create a domain with named boundaries "left", "right", "top" and "bottom"
 domain = anuga.rectangular_cross_domain(int(length/dx), int(width/dy),
@@ -26,7 +26,7 @@ domain.set_name('spiral_wall')               # Output name
 #------------------------------------------------------------------------------
 # Setup initial conditions
 #------------------------------------------------------------------------------
-def topography(x, y):
+def topography_circlewall(x, y):
 
     z = x * 0.   # Flat surface
     
@@ -36,9 +36,6 @@ def topography(x, y):
         
     N = len(x)
     for i in range(N):
-    
-        
-        # Make circular wall 
 
         # Distance from centre to this point        
         d = sqrt((x[i] - c[0])**2 + (y[i] - c[1])**2)
@@ -54,6 +51,42 @@ def topography(x, y):
                 
         x_outer = r_outer * cos(theta) + c[0]
         y_outer = r_outer * sin(theta) + c[1]        
+        d_outer = sqrt((x_outer - c[0])**2 + (y_outer - c[1])**2) 
+
+        # Raise elevation for points between inner and outer distance
+        if d_inner < d < d_outer:
+            z[i] += 2                
+        
+    return z            
+    
+def topography(x, y):
+
+    z = x * 0.   # Flat surface
+    
+    c = (5, 5)   # Centre
+    r_inner = 1
+    r_outer = 2
+        
+    N = len(x)
+    for i in range(N):
+
+        # Distance from centre to this point        
+        d = sqrt((x[i] - c[0])**2 + (y[i] - c[1])**2)
+        
+        # Angle from centre to this point
+        cos_theta = (x[i] - c[0]) / d     
+        theta = acos(cos_theta)
+        
+
+        # Find distances from centre to inner and outer periphery along this vector
+        r = r_inner * theta * 0.5  # spiral form        
+        x_inner = r * cos(theta) + c[0]
+        y_inner = r * sin(theta) + c[1]                
+        d_inner = sqrt((x_inner - c[0])**2 + (y_inner - c[1])**2)         
+                
+        r = r_outer * theta * 0.5  # spiral form
+        x_outer = r * cos(theta) + c[0]
+        y_outer = r * sin(theta) + c[1]        
         d_outer = sqrt((x_outer - c[0])**2 + (y_outer - c[1])**2) 
 
         # Raise elevation for points between inner and outer distance
@@ -79,5 +112,5 @@ domain.set_boundary({'left': Br, 'right': Bi, 'top': Br, 'bottom': Br})
 #------------------------------------------------------------------------------
 # Evolve system through time
 #------------------------------------------------------------------------------
-for t in domain.evolve(yieldstep=0.2, finaltime=0.2):
+for t in domain.evolve(yieldstep=0.2, finaltime=0):
     domain.print_timestepping_statistics()
